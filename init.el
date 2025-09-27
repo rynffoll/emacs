70b5fb7781f5cff41f9785d6bb1849ec2b6db0de
Ruslan Kamashev <rynffoll@gmail.com>
2025-09-27 23:11:47 +0400

    Update flycheck config: remove unused parts

--- init.el
+++ init.el
@@ -1550,43 +1550,6 @@
   (after-init-hook . global-flycheck-mode)
   (flycheck-mode-hook . +flycheck-set-indication-mode)
   :config
-  ;; (when (display-graphic-p)
-  ;;   (define-fringe-bitmap '+flycheck-fringe-indicator
-  ;;     (vector #b00000000
-  ;;             #b00000000
-  ;;             #b00000000
-  ;;             #b00000000
-  ;;             #b00000000
-  ;;             #b00000100
-  ;;             #b00001100
-  ;;             #b00011100
-  ;;             #b00111100
-  ;;             #b00011100
-  ;;             #b00001100
-  ;;             #b00000100
-  ;;             #b00000000
-  ;;             #b00000000
-  ;;             #b00000000
-  ;;             #b00000000
-  ;;             #b00000000))
-
-  ;;   (flycheck-define-error-level 'error
-  ;;     :severity 2
-  ;;     :overlay-category 'flycheck-error-overlay
-  ;;     :fringe-bitmap '+flycheck-fringe-indicator
-  ;;     :fringe-face 'flycheck-fringe-error)
-
-  ;;   (flycheck-define-error-level 'warning
-  ;;     :severity 1
-  ;;     :overlay-category 'flycheck-warning-overlay
-  ;;     :fringe-bitmap '+flycheck-fringe-indicator
-  ;;     :fringe-face 'flycheck-fringe-warning)
-
-  ;;   (flycheck-define-error-level 'info
-  ;;     :severity 0
-  ;;     :overlay-category 'flycheck-info-overlay
-  ;;     :fringe-bitmap '+flycheck-fringe-indicator
-  ;;     :fringe-face 'flycheck-fringe-info))
   (flycheck-redefine-standard-error-levels "!" 'exclamation-mark))
 
 (use-package consult-flycheck
